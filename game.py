94cf6a70f45feb42de626287e3d69d933442fb2b
AshtonA24 <ashton.assareh@gmail.com>
2023-12-10 22:06:52 -0800

    yuh

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 pygame.init()
 screen = pygame.display.set_mode((1400,800))
 pygame.display.set_caption("GAME")
@@ -24,6 +25,7 @@ class Player(pygame.sprite.Sprite):
         self.left_side_click = False
         self.speed = 6
         self.moving = False
+        self.swipe_timer = 0
 
     def player_input(self):
         global is_swipe, sword_swipe_group, sword_swipe
@@ -42,8 +44,9 @@ class Player(pygame.sprite.Sprite):
         if not keys[pygame.K_w] and not keys[pygame.K_a] and not keys[pygame.K_s] and not keys[pygame.K_d]: self.moving = False
         else:self.moving = True
         #mouse
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] and self.swipe_timer < 0:
             is_swipe = True
+            self.swipe_timer = 30
             if not sword_swipe_group:
                 sword_swipe_group.add(sword_swipe)
             if pygame.mouse.get_pos()[0] < self.rect.center[0]:
@@ -79,28 +82,27 @@ class Player(pygame.sprite.Sprite):
 class SwordSwipe(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.swipe1 = pygame.transform.scale_by(pygame.image.load('graphics/sword/1.png'),(0.3))
-        self.swipe2 = pygame.transform.scale_by(pygame.image.load('graphics/sword/2.png'),(0.3))
-        self.swipe3 = pygame.transform.scale_by(pygame.image.load('graphics/sword/3.png'),(0.3))
-        self.swipe4 = pygame.transform.scale_by(pygame.image.load('graphics/sword/4.png'),(0.3))
-        self.swipe5 = pygame.transform.scale_by(pygame.image.load('graphics/sword/5.png'),(0.3))
-        self.swipe6 = pygame.transform.scale_by(pygame.image.load('graphics/sword/6.png'),(0.3))
-        self.swipe7 = pygame.transform.scale_by(pygame.image.load('graphics/sword/7.png'),(0.3))
-        self.swipe8 = pygame.transform.scale_by(pygame.image.load('graphics/sword/8.png'),(0.3))
-        self.x = player.rect.center[0]
-        self.y = player.rect.center[1]
 
-        self.frames = [self.swipe1, self.swipe2, self.swipe3, self.swipe4, self.swipe5, self.swipe6, self.swipe7, self.swipe8]
+        self.blank = pygame.image.load('graphics/sword/Empty2.png')
+        self.swipe1 = pygame.transform.scale_by(pygame.image.load('graphics/sword/1 - copy.png'),(0.3))
+        self.swipe2 = pygame.transform.scale_by(pygame.image.load('graphics/sword/2 - copy.png'),(0.3))
+        self.swipe3 = pygame.transform.scale_by(pygame.image.load('graphics/sword/3 - copy.png'),(0.3))
+        self.swipe4 = pygame.transform.scale_by(pygame.image.load('graphics/sword/4 - copy.png'),(0.3))
+        self.swipe5 = pygame.transform.scale_by(pygame.image.load('graphics/sword/5 - copy.png'),(0.3))
+        self.swipe6 = pygame.transform.scale_by(pygame.image.load('graphics/sword/6 - copy.png'),(0.3))
+        self.swipe7 = pygame.transform.scale_by(pygame.image.load('graphics/sword/7 - copy.png'),(0.3))
+        self.swipe8 = pygame.transform.scale_by(pygame.image.load('graphics/sword/8 - copy.png'),(0.3))
+
+        self.frames = [self.blank, self.swipe1, self.swipe2, self.swipe3, self.swipe4, self.swipe5, self.swipe6, self.swipe7, self.swipe8]
         self.index = 0
-        self.image = self.frames[self.index]
-
-        self.rect = self.image.get_rect(center = (self.x,self.y))
+        self.image = self.blank
+        self.rect = self.image.get_rect(center = (player.rect.center[0],player.rect.center[1]))
 
     def animation(self):
         global is_swipe
         global sword_swipe
         if is_swipe and player.left_side_click:
-            self.index += 0.5
+            self.index += 1
             if self.index >= len(self.frames): 
                 self.index = 0
                 is_swipe = False
@@ -108,7 +110,7 @@ class SwordSwipe(pygame.sprite.Sprite):
             self.image = self.frames[int(self.index)]
             self.image = pygame.transform.flip(self.image, True, False)
         elif is_swipe:
-            self.index += 0.5
+            self.index += 1
             if self.index >= len(self.frames): 
                 self.index = 0
                 is_swipe = False
@@ -116,21 +118,34 @@ class SwordSwipe(pygame.sprite.Sprite):
             self.image = self.frames[int(self.index)]
     
     def check_move(self):
+        global is_swipe
         if player.left_side_click:
-            self.rect.center = (player.rect.center[0]-50, player.rect.center[1])
+            self.rect.midright = (player.rect.midleft[0], player.rect.center[1])
         else:
-            self.rect.center = (player.rect.center[0]+50, player.rect.center[1])
+            self.rect.midleft = (player.rect.midright[0], player.rect.center[1])
+        
+        
     def update(self):
         self.check_move()
         self.animation()
+        
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.stand = pygame.transform.scale_by(pygame.image.load('graphics/enemy/stand.png'),(0.2))
 
+        self.frames = [self.stand]
+        self.index = 0
+        self.image = self.frames[self.index]
 
-    
+        self.rect = self.image.get_rect(center = (randint(0,1400),randint(400,700)))
+        self.left = False
+        self.left_side_click = False
+        self.speed = 6
+        self.moving = False
 
-
-
-
-
+def player_enemy_collision():
+    return pygame.sprite.spritecollide(player, enemy_group, False)
 rescale = 800/2160
 background = pygame.transform.scale_by(pygame.image.load('graphics/background/game_background_4.png').convert_alpha(),rescale)
 background_rocks = pygame.transform.scale_by(pygame.image.load('graphics/background/front_decor.png').convert_alpha(),rescale)
@@ -139,8 +154,16 @@ player = Player()
 player_group = pygame.sprite.GroupSingle()
 player_group.add(player)
 
-sword_swipe_group = pygame.sprite.GroupSingle()
 
+enemy_group = pygame.sprite.Group()
+for i in range (10):
+    enemy_group.add(Enemy())
+    if player_enemy_collision():
+        enemy_group.remove(enemy_group.sprites()[-1])
+
+
+sword_swipe = SwordSwipe()
+sword_swipe_group = pygame.sprite.GroupSingle()
 
 game_run = True
 
@@ -149,20 +172,30 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     sword_swipe_group.add(sword_swipe)
+        
     if game_run:
 
-        sword_swipe = SwordSwipe()
-
-
         screen.blit(background,(0,0))
+        # pygame.draw.rect(screen, (0,0,255), sword_swipe.rect)
+        enemy_group.draw(screen)
+        enemy_group.update()
         player_group.draw(screen)
         player_group.update()
         sword_swipe_group.draw(screen)
         sword_swipe_group.update()
+        pygame.sprite.spritecollide(sword_swipe, enemy_group, is_swipe)
+        pygame.sprite.spritecollide(sword_swipe, enemy_group, is_swipe)
         screen.blit(background_rocks,(0,0))
-        clock.tick(60)
+        player.swipe_timer -= 1
+        pygame.sprite.spritecollide(sword_swipe, enemy_group, is_swipe)
+        if player_enemy_collision():
+            game_run = False
+
+    else:
+        screen.fill((255,255,255))
+
+    pygame.display.update()
+    clock.tick(60)
         
-        pygame.display.update()
+        
     
