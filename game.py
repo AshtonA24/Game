53a4ef54d42f155e494eadc84e25d71c9134ed80
AshtonA24 <ashton.assareh@gmail.com>
2023-12-12 19:22:38 -0800

    as

--- game.py
+++ game.py
@@ -196,7 +196,7 @@ class Ogre(pygame.sprite.Sprite):
         x = self.rect.center[0] - player.rect.center[0]
         y = self.rect.center[1] - player.rect.center[1]
         angle = math.atan2(y, x)
-        speed = 5
+        speed = 3
         move_x = speed * math.cos(angle)
         move_y = speed * math.sin(angle)
 
@@ -258,7 +258,7 @@ class Ogre(pygame.sprite.Sprite):
 
     def animation(self):
         if self.moving:
-            self.index_walk += 0.15
+            self.index_walk += 0.1
             if self.index_walk >= len(self.frames_walk):
                 self.index_walk = 0
             self.image = self.frames_walk[int(self.index_walk)]
@@ -303,10 +303,24 @@ class Mage(pygame.sprite.Sprite):
         self.walk10 = pygame.transform.scale_by(pygame.image.load('graphics/mage/walk/10.png'),(0.2))
         self.walk11 = pygame.transform.scale_by(pygame.image.load('graphics/mage/walk/11.png'),(0.2))
         self.walk12 = pygame.transform.scale_by(pygame.image.load('graphics/mage/walk/12.png'),(0.2))
+
+        self.attack1 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/1.png'),(0.2))
+        self.attack2 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/2.png'),(0.2))
+        self.attack3 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/3.png'),(0.2))
+        self.attack4 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/4.png'),(0.2))
+        self.attack5 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/5.png'),(0.2))
+        self.attack6 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/6.png'),(0.2))
+        self.attack7 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/7.png'),(0.2))
+        self.attack8 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/8.png'),(0.2))
+        self.attack9 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/9.png'),(0.2))
+        self.attack10 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/10.png'),(0.2))
+        self.attack11 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/11.png'),(0.2))
+        self.attack12 = pygame.transform.scale_by(pygame.image.load('graphics/mage/attack/12.png'),(0.2))
         
 
         self.frames_walk = [self.walk1, self.walk2, self.walk3, self.walk4, self.walk5, self.walk6, self.walk7, self.walk8, self.walk9, self.walk10, self.walk11, self.walk12]
-        # self.frames_attack = [self.attack1, self.attack2, self.attack3, self.attack4, self.attack5, self.attack6, self.attack7]
+        self.frames_attack = [self.attack1, self.attack2, self.attack3, self.attack4, self.attack5, self.attack6, self.attack7, self.attack8]
+        self.attack_timer = 0
         self.index_attack = 0
         self.index_walk = 0
         self.image = self.frames_walk[self.index_walk]
@@ -328,7 +342,7 @@ class Mage(pygame.sprite.Sprite):
         self.attacking = False
         self.i_frame = False
         self.i_frame_timer = 0
-        self.radius = 250
+        self.radius = 400
         
         
     def move(self):
@@ -347,8 +361,8 @@ class Mage(pygame.sprite.Sprite):
         if x < 0: self.left = False
         else: self.left = True
 
-        # if self.attacking: self.moving = False
-        # else: self.moving = True
+        if self.attacking: self.moving = False
+        else: self.moving = True
 
 
         # Calculate the new position based on the angle and speed
@@ -373,10 +387,15 @@ class Mage(pygame.sprite.Sprite):
     def check_attack(self):
         distance = math.hypot(player.rect.center[0] - self.rect.center[0], player.rect.center[1] - self.rect.center[1])
         if abs(distance) < self.radius:
-            self.moving = False
-        else:self.moving = True
-        
-
+            self.attacking = True
+    
+    def shoot_ball(self):
+        global projectile_group
+        speed = 10
+        angle = math.atan2(player.rect.center[1]-self.rect.center[1], player.rect.center[0]-self.rect.center[0])
+        dx = math.cos(angle) * speed
+        dy = math.sin(angle) * speed
+        projectile_group.add(MageBall(self.rect.center, dx, dy))
     
     def check_health(self):
         if self.health <= 0:
@@ -403,16 +422,21 @@ class Mage(pygame.sprite.Sprite):
             if self.left:
                 self.image = pygame.transform.flip(self.image, True, False)
 
-        # if self.attacking:
-        #     self.index_attack += 0.35
-        #     if self.index_attack >= len(self.frames_attack):
-        #         self.index_attack = 0
-        #         self.attacking = False
-        #         if self.attack_rect.colliderect(player.rect):
-        #             player.dead = True
-        #     self.image = self.frames_attack[int(self.index_attack)]
-        #     if self.left:
-        #         self.image = pygame.transform.flip(self.image, True, False)
+        if self.attacking and self.attack_timer <= 0:
+            self.index_attack += 0.15
+            
+            if self.index_attack >= len(self.frames_attack):
+                self.index_attack = 0
+                self.shoot_ball()
+                self.attack_timer = 120
+                self.attacking = False
+                if self.attack_rect.colliderect(player.rect):
+                    player.dead = True
+            self.image = self.frames_attack[int(self.index_attack)]
+            if self.left:
+                self.image = pygame.transform.flip(self.image, True, False)
+
+        self.attack_timer -= 1
 
 
     def update(self):
@@ -425,6 +449,23 @@ class Mage(pygame.sprite.Sprite):
             self.check_health()
             self.check_dead()
 
+class MageBall(pygame.sprite.Sprite):
+    def __init__(self, start, dx, dy):
+        super().__init__()
+
+        orb = pygame.image.load('graphics/projectiles/mage/orb.png')
+        self.image = orb
+        self.rect = self.image.get_rect(center = start)
+        self.dx = dx
+        self.dy = dy
+
+    def move(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+    def update(self):
+        self.move()
+
             
 def restart_game():
     global game_run, enemy_group
@@ -434,7 +475,7 @@ def restart_game():
     player.rect.center = (700,500)
     player.dead = False
     player.full_dead = False
-    enemy_group.add(Ogre(True))
+    # enemy_group.add(Ogre(True))
     enemy_group.add(Mage(False))
 #blackground
 rescale = 800/2160
@@ -447,6 +488,7 @@ player = Player()
 player_group = pygame.sprite.GroupSingle()
 player_group.add(player)
 enemy_group = pygame.sprite.Group()
+projectile_group = pygame.sprite.Group()
 
 
 
@@ -486,6 +528,8 @@ while True:
         enemy_group.update()
         player_group.draw(screen)
         player_group.update()
+        projectile_group.update()
+        projectile_group.draw(screen)
         sword_swipe_group.draw(screen)
         sword_swipe_group.update()
         screen.blit(background_rocks,(0,0))
